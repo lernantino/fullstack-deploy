ccd3f7181956032697ad434377596b011eead946
lernantino <fs-online@2u.com>
2024-07-29 18:55:39 -0600

    updates server for production

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,5 +1,4 @@
 import express from 'express';
-import path from 'node:path';
 
 import db from './config/connection.js';
 import routes from './routes/index.js';
@@ -14,10 +13,10 @@ app.use(express.json());
 app.use(routes);
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/dist')));
+  app.use(express.static('../client/dist'));
 
    app.get('*', (_req, res) => {
-    res.sendFile(path.join(__dirname, '../client/dist/index.html'));
+    res.sendFile('../client/dist/index.html');
   });
 }
 
